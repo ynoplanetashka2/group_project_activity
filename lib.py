00328af95468b6cc631bbc86b9978b3340792507
ynoplanetashka2 <mrynoplanetashka@gmail.com>
2023-06-19 02:32:38 +0500

    base features

---
+++ lib.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def solve_de_inner(**argv):
+    step = argv['step']
+    start = argv['start']
+    end = argv['end']
+    operator = argv['operator']
+    initial = argv['initial']
+
+    current = initial
+    time = start
+    while time <= end:
+        delta = np.matmul(operator(time), current) * step
+        current += delta
+        time += step
+    
+    return current
+
+def solve_de(**argv):
+    omega = argv['omega']
+    cos_coef = argv['cos_coef']
+    coef = lambda time: - omega * omega * (1 + cos_coef * np.cos(time))
+    columns = []
+    initials = [
+        np.array([1, 0], float),
+        np.array([0, 1], float),
+    ]
+    for initial in initials:
+        columns.append(
+            solve_de_inner(
+                step = .001,
+                start = 0,
+                end = 2 * np.pi,
+                initial = initial,
+                operator = lambda time: np.array([[0, 1], [coef(time), 0]], float),
+            )
+        )
+    
+    return np.array(columns)
+
+def is_stable(**argv):
+    sol = solve_de(**argv)
+    eigvals = np.linalg.eigvals(sol)
+    for i in range(2):
+        eigen = eigvals[i]
+        abs_val = np.abs(eigen)
+        if abs_val <= 1:
+            return True
+    return False
